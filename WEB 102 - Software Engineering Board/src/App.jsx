66e5f1fd3bc4a4b86438f69c8f3a17bc0c0952f5
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-02-25 11:19:30 -0500

    Created Vite files. Set background image

---
+++ WEB 102 - Software Engineering Board/src/App.jsx
@@ -0,0 +1,17 @@
+// Imports the CSS file with code that styles of App or the root of the application
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+    
+
+      
+      <h1>Software Engineering Favorites</h1>
+
+    </div>
+  )
+}
+
+export default App
