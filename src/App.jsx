5bbe4cfe0a26a88694a72c5a545ffb0ba6c3f01d
Mariia Onokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-02-25 17:33:38 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// Imports the CSS file with code that styles of App or the root of the application
+import './App.css'
+
+import CardGrid from './components/CardGrid'
+
+const App = () => {
+
+  return (
+    <div className="App">
+
+      <h1>Software Engineering Favorites</h1>
+
+      <CardGrid />
+
+    </div>
+  )
+}
+
+export default App
