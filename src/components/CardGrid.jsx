ed321f3269c3fa5afb1b8da30d5aa7ab9c35e19d
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-02-26 14:22:23 -0500

    Added more resources (total 12)

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -38,6 +38,12 @@ const CardGrid = () => {
                         <Card name="LeetCode" link="https://leetcode.com/" picture="https://cdn.cdo.mit.edu/wp-content/uploads/sites/67/2021/01/0_zuhXdNAIUoxEem4-.png" />
                     </tr>
 
+                    {/* Grid row */}
+                        <Card name="Font Awesome" link="https://fontawesome.com/" picture="https://www.drupal.org/files/project-images/font_awesome_logo.png" />
+
+                        <Card name="Forage" link="https://www.theforage.com/" picture="https://miro.medium.com/max/1200/1*Df5aldU_dXvFRsNQbGYLLA.png" />
+
+                        <Card name="Real Python" link="https://realpython.com/" picture="https://files.realpython.com/media/real-python-logo-square.28474fda9228.png" />
                 </tbody>
             </table>
             
